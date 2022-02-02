65a2fe0792cc2bb523c9dfdcd5ec2bd67e74722b
KianPinocchio <k.soofizadeh@icloud.com>
2022-02-02 18:50:08 +0100

    Create a data simulation
    Used a simulation based power analysis procedure to simulate the outcome of the Study 1 (Troy et al., 2017). This version works.

---
+++ Data_simulation.R
@@ -0,0 +1,122 @@
+
+## Problem:
+## 1.To decide on an appropriate Standard Deviation of Error 
+#   for the simulated data based on data from an existing study.
+# I will be using the approach outlined here https://github.com/kekecsz/Simulation_based_power_analysis.git
+
+## The outcome (CES-D-5) in study (Troy et al., 2017) had the following  properties: (M = 3.46, SD = 3.34, alpha =.81)
+# In addition the study reports total number of participants (N_total) 
+# and the regression coefficients of all variables needed to simulate data and 
+# estimate the error of the outcome variable.
+
+## Reference: Troy, A. S., Ford, B. Q., McRae, K., Zarolia, P., & Mauss, I. B. (2017). 
+#   Change the things you can: Emotion regulation is more beneficial for people from lower than from higher socioeconomic status. 
+#     Emotion, 17(1), 141–154. https://doi.org/10.1037/emo0000210
+
+# install packages ##############################
+pacman::p_load(pacman, tidyverse, truncnorm, Runuran, dplyr)
+
+
+# simulation function ###################
+data_simulation_function = function(N_total, 
+                                    CRA_coefficient,
+                                    SES_coefficient,
+                                    PSS_coefficient,
+                                    int_coefficient,
+                                    error_sd){
+  
+  #cognitive reappraisal ability (CRA)
+  CRA = rtruncnorm(n = N_total, a=1, b=7, mean = 5.12, sd = 1.21)
+  CRA = round(CRA)
+  
+  #Socioeconomic Status (SES)
+  SES = rtruncnorm(n = N_total, a=1, b=12, mean = 5.27, sd = 3.06)
+  SES = round(SES)
+  
+  #Perceived Stress Scale (PSS)
+  PSS = rtruncnorm(n = N_total, a=1, b=5, mean = 2.70, sd = 0.9)
+  PSS = round(PSS)
+  
+  #error
+  error = rnorm(n = N_total, m = 0, sd = error_sd)
+  
+  #interaction of CRA and SES
+  interaction = (CRA * SES * int_coefficient)
+  
+  # Depressive Symptoms (CES-D)
+  outcome = (CRA*CRA_coefficient + 
+               SES*SES_coefficient + 
+               interaction +
+               PSS*PSS_coefficient + 
+               error)
+  
+  # Storing all simualted data together
+  simulated_data = data.frame(outcome = outcome,
+                              CRA = CRA,
+                              SES = SES,
+                              PSS = PSS)
+  
+  return(simulated_data) }
+
+## Here we replicate the simulation function a high number of times
+#   to get a large sample of various Standard Deviations of our
+#   simulations
+error_deviation_simulation_function = function(N_total, 
+                                               CRA_coefficient,
+                                               SES_coefficient,
+                                               PSS_coefficient,
+                                               int_coefficient,
+                                               error_sd,
+                                               nr_iterations){
+  
+  # replicate the simulation function
+  data = data.frame(replicate( n = nr_iterations,
+                               data_simulation_function(N_total, 
+                                                        CRA_coefficient,
+                                                        SES_coefficient,
+                                                        PSS_coefficient,
+                                                        int_coefficient,
+                                                        error_sd)))
+  
+  ## Steps to make outcome data readable to visualise by hist() later on
+  # 1.extract outcome data
+  outcome <- data.frame(data["outcome",])
+  
+  # 2.extract outcome observation indexes / column names
+  outcome_observations = c(colnames(outcome))
+  
+  # 3.create a readable dataframe of outcome data
+  outcome <- outcome %>% 
+    unchop(all_of(outcome_observations))
+  
+  # 4.calculate Standard Deviation of all outcome observations
+  outcome <- apply(outcome,2,sd)
+  
+  return(outcome)
+}
+
+# create an array of standard deviations of all the outcomes that we simulated
+all_outcome_observations_SD = error_deviation_simulation_function(N_total = 301,
+                                                                  CRA_coefficient = -0.15,
+                                                                  SES_coefficient = -0.03,
+                                                                  PSS_coefficient = 0.53,
+                                                                  int_coefficient = 0.09,
+                                                                  error_sd = 3,
+                                                                  nr_iterations = 100)
+## summarise our results: We are looking for a Mean close to the 
+#  Standard Deviation of the outcome reported by the study (SD = 3.34)
+summary(all_outcome_observations_SD)
+
+# visualise all observations.
+hist(all_outcome_observations_SD)
+
+# CLEAN UP #################################################
+
+# Clear environment
+rm(list = ls()) 
+
+# Clear packages
+p_unload(all)
+
+# Clear console
+cat("\014")
