f194bbdbf93dedc92d82d9fb2e076805c054fb95
KianPinocchio <k.soofizadeh@icloud.com>
2022-02-04 16:00:07 +0100

    Simulation complete
    Simulated data and calculated the power. The power seems to be quite low for the original model ( ~ 0.5).

--- Data_simulation.R
+++ Data_simulation.R
@@ -1,9 +1,10 @@
 
-## Problem:
+## Objectives:
 ## 1.To decide on an appropriate Standard Deviation of Error 
 #   for the simulated data based on data from an existing study.
-# I will be using the approach outlined here https://github.com/kekecsz/Simulation_based_power_analysis.git
-
+#   I will be using the approach outlined here https://github.com/kekecsz/Simulation_based_power_analysis.git
+# 
+# 2. Use the estimated SD to simulate the data and run a simulation based Power Analysis
 ## The outcome (CES-D-5) in Study 1 (Troy et al., 2017) had the following  properties: (M = 3.46, SD = 3.34, alpha =.81)
 # In addition the study reports total number of participants (N_total) 
 # and the regression coefficients of all variables needed to simulate data and 
@@ -13,17 +14,17 @@
 #   Change the things you can: Emotion regulation is more beneficial for people from lower than from higher socioeconomic status. 
 #     Emotion, 17(1), 141–154. https://doi.org/10.1037/emo0000210
 
-# install packages ##############################
+# install packages ######################
 pacman::p_load(pacman, tidyverse, truncnorm, Runuran, dplyr)
 
 
 # simulation function ###################
-data_simulation_function = function(N_total, 
+data_simulation_function <- function(N_total, 
                                     CRA_coefficient,
                                     SES_coefficient,
                                     PSS_coefficient,
                                     int_coefficient,
-                                    error_sd){
+                                    sd_error){
   
   #cognitive reappraisal ability (CRA)
   CRA = rtruncnorm(n = N_total, a=1, b=7, mean = 5.12, sd = 1.21)
@@ -38,7 +39,7 @@ data_simulation_function = function(N_total,
   PSS = round(PSS)
   
   #error
-  error = rnorm(n = N_total, m = 0, sd = error_sd)
+  error = rnorm(n = N_total, m = 0, sd = sd_error)
   
   #interaction of CRA and SES
   interaction = (CRA * SES * int_coefficient)
@@ -61,12 +62,12 @@ data_simulation_function = function(N_total,
 ## Here we replicate the simulation function a high number of times
 #   to get a large sample of various Standard Deviations of our
 #   simulations
-error_deviation_simulation_function = function(N_total, 
+error_simulation_function <- function(N_total, 
                                                CRA_coefficient,
                                                SES_coefficient,
                                                PSS_coefficient,
                                                int_coefficient,
-                                               error_sd,
+                                               sd_error,
                                                nr_iterations){
   
   # replicate the simulation function
@@ -76,7 +77,7 @@ error_deviation_simulation_function = function(N_total,
                                                         SES_coefficient,
                                                         PSS_coefficient,
                                                         int_coefficient,
-                                                        error_sd)))
+                                                        sd_error)))
   
   ## Steps to make outcome data readable to visualise by hist() later on
   # 1.extract outcome data
@@ -96,19 +97,66 @@ error_deviation_simulation_function = function(N_total,
 }
 
 # create an array of standard deviations of all the outcomes that we simulated
-all_outcome_observations_SD = error_deviation_simulation_function(N_total = 301,
-                                                                  CRA_coefficient = -0.15,
-                                                                  SES_coefficient = -0.03,
-                                                                  PSS_coefficient = 0.53,
-                                                                  int_coefficient = 0.09,
-                                                                  error_sd = 3,
-                                                                  nr_iterations = 100)
+all_outcome_observations_SD = error_simulation_function(N_total = 301,
+                                                        CRA_coefficient = -0.15,
+                                                        SES_coefficient = -0.03,
+                                                        PSS_coefficient = 0.53,
+                                                        int_coefficient = 0.09,
+                                                        sd_error = 3, # Standard Deviation of error
+                                                        nr_iterations = 100)
 ## summarise our results: We are looking for a Mean close to the 
 #  Standard Deviation of the outcome reported by the study (SD = 3.34)
 summary(all_outcome_observations_SD)
 
 # visualise all observations.
 hist(all_outcome_observations_SD)
+
+#### Regression Analysis ####
+# Using the estimated SD (SD = 3) we run a simulation based power analysis below
+# Model is based on the study: entering all IVs at the same time
+analysis_function_regression <- function(data){ 
+  regression = lm(outcome ~ CRA + SES + CRA:SES + PSS, data = data)
+  p_value = summary(regression)$coefficients[2,4]
+  
+  decision = if(p_value < 0.05){"H1"} else {"Inconclusive"}
+  
+  return(decision) }
+
+### Analysis and Simulation function together
+analysis_and_simulation_function <- function(N_total, 
+                                             CRA_coefficient,
+                                             SES_coefficient,
+                                             PSS_coefficient,
+                                             int_coefficient,
+                                             sd_error){
+  
+  simulated_data = data_simulation_function(N_total, 
+                                            CRA_coefficient,
+                                            SES_coefficient,
+                                            PSS_coefficient,
+                                            int_coefficient,
+                                            sd_error)
+  
+  decision = analysis_function_regression(data = simulated_data)
+  
+  return(decision)
+}
+
+# Number of times we iterate the Simulation and Analysis
+number_iterations = 100
+
+## Iterate all functions
+## Save all H1 accepted results
+all_decisions = replicate(n = number_iterations, 
+                          analysis_and_simulation_function(N_total = 100, 
+                                                            CRA_coefficient = -0.15,
+                                                            SES_coefficient = -0.03,
+                                                            PSS_coefficient = 0.53,
+                                                            int_coefficient = 0.09,
+                                                            sd_error = 3))
+
+## Percentage of times H1 accepted
+sum(all_decisions == "H1")/length(all_decisions)
 
 # CLEAN UP #################################################
 
