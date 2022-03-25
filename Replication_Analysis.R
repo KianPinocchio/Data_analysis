f2dcb6c12deaa0328b49701c08106d0dfaf8aac2
KianPinocchio <k.soofizadeh@icloud.com>
2022-03-25 18:16:08 +0430

    Descriptives and Alphas

--- Replication_Analysis.R
+++ Replication_Analysis.R
@@ -3,7 +3,7 @@
 # Date: March 25th, 2022
 
 pacman::p_load(pacman, tidyverse, kableExtra, psych, janitor, car, performance, see,
-  gridExtra, interactions, devtools)
+  gridExtra, interactions, devtools,rmarkdown, knitr)
 
 library(readxl) # for importing excel file
 
@@ -26,11 +26,7 @@ summary(clean_data)
 clean_data <- rename(clean_data, "gender" = "Sex")
 
 # Check demographics
-clean_data$gender <- factor(clean_data$gender, levels=c(1,2,3,4),
-                          labels=c("Male", 
-                                   "Female",
-                                   "non-Binary",
-                                   "IDWA"))
+clean_data$gender <- factor(clean_data$gender, levels=c(1,2,3,4))
 summary (clean_data$gender)
 summary (clean_data$Age)
 
@@ -57,7 +53,8 @@ mcq_csc <- c("MCQ3","MCQ5","MCQ12",
 scored_data <- clean_data %>%
   
   #row mean of the selected columns
-  mutate(across(c("PSS2", "PSS3"), ~{6 - .}), # first recode PSS items 2 & 3 (6 - response)
+  mutate(across(c("PSS2", "PSS3"), ~{6 - .}), # recode PSS items 2 & 3 (6 - response)
+         across(c("BSM1", "BSM3", "BSM7", "BSM8"), ~{8 - .}), # first recode BSM items 1, 3, 7, 8 (8 - response)
          mean_cra = rowMeans(select(., starts_with("CRA"))),
          mean_hcru = rowMeans(select(., starts_with("HCRU"))),
          mean_pss = rowMeans(select(., starts_with("PSS"))),
@@ -79,7 +76,94 @@ scored_data <- clean_data %>%
   select(id, Age, SES, gender, mean_cra, mean_hcru,
          sum_cesd, mean_pss, mean_bsm, sum_pos, sum_neg, sum_cc, sum_nc, sum_csc, sum_MCQ)
 
+# Descriptives
 
+# In the first step, the data are summarized to get the descriptive statistics.
+#Subsequently, the data are reformatted.
+descriptives <- scored_data %>% 
+  dplyr::summarize(across(c(SES, Age, mean_cra, mean_hcru,sum_cesd, 
+                            mean_pss, mean_bsm, sum_pos, 
+                            sum_neg, sum_cc, sum_nc, sum_csc, sum_MCQ),
+                   list(mean = mean, sd = sd, min = min, max = max))) %>%
+  
+  # bring everything in long format
+  pivot_longer(everything(), names_to = "name") %>%
+  
+  # separate names at last underscore
+  separate(name, into = c("name","descriptive"), sep = "_(?=[^_]+$)") %>%
+  
+  # get into a bit wider format again
+  pivot_wider(names_from = name, values_from = value) %>%
+  
+  # rename to have nicer column names
+  rename(Summary = descriptive,
+         CRA = mean_cra,  #
+         HCRU = mean_hcru, #
+         PSS = mean_pss,  #
+         CESD = sum_cesd, #
+         BSM = mean_bsm,  #
+         POS = sum_pos,  #
+         NEG = sum_neg, # 
+         CC = sum_cc, #
+         NC = sum_nc, #
+         CSC = sum_csc, #
+         MCQT = sum_MCQ) # MCQ =Meta Cognitions Questionnaire Total
+
+# Calculate cronbach’s alphas
+# Select the items from the raw data that belong to the specific scale.
+# calculate alpha and extract raw_alpha from the list the alpha function generates.
+alpha <- clean_data %>%
+  dplyr::summarize(
+            # Replication Block Alphas
+            cra_alpha = select(.,starts_with("CRA")) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"), # extract total and then raw_alpha from list
+            hcru_alpha = select(.,starts_with("HCRU")) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            cesd_alpha = select(.,starts_with("CES_D")) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            pss_alpha = select(.,starts_with("PSS")) %>% psych::alpha(check.keys=TRUE) %>%
+              pluck("total", "raw_alpha"),
+            
+            # BSM Alphas
+            BSM_alpha = select(.,starts_with("BSM")) %>% psych::alpha(check.keys=TRUE) %>%
+              pluck("total", "raw_alpha"),
+            
+            # MCQ scale Alphas
+            pos_alpha = select(.,all_of(mcq_pos)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            neg_alpha = select(.,all_of(mcq_neg)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            cc_alpha = select(.,all_of(mcq_cc)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            nc_alpha = select(.,all_of(mcq_nc)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            csc_alpha = select(.,all_of(mcq_csc)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"),
+            mcq_alpha = select(.,all_of(mcq_csc)) %>% psych::alpha() %>%
+              pluck("total", "raw_alpha"))
+
+# add alphas as extra row to the descriptives table
+descriptives <- descriptives %>%
+  add_row(Summary = "alpha", SES = NA, CRA = alpha$cra_alpha, HCRU = alpha$hcru_alpha,
+          PSS = alpha$pss_alpha, CESD = alpha$cesd_alpha, BSM = alpha$BSM_alpha,
+          POS = alpha$pos_alpha, NEG = alpha$neg_alpha, CC = alpha$cc_alpha,
+          NC = alpha$nc_alpha, CSC = alpha$csc_alpha, MCQT = alpha$mcq_alpha)
+
+# make it a nicely formatted table
+library(rmarkdown)
+apa_table(descriptives) # is only shown when RMarkdown document is knitted
+
+# Plot monthly family income
+income_plot<-hist(scored_data$SES,
+                  main="Family income distribution",
+                  xlab="family income category")
+summary(scored_data$SES)
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+Mode(scored_data$SES)
 
 # CLEAN UP #################################################
 
