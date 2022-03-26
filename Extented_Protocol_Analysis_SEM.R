7584c60d335bf2b2f3f734f424a4442672958b99
KianPinocchio <k.soofizadeh@icloud.com>
2022-03-26 15:49:35 +0430

    Extension block data analysis
    The data analysis code for the extension block of the study that came after the replication block. I will use structural equation modelling to test and explore the hypotheses specified in the protocol.

---
+++ Extented_Protocol_Analysis_SEM.R
@@ -0,0 +1,7 @@
+---
+title: "Data analysis for the extension protocol of study: 
+The interplay of cognitive reappraisal ability, 
+socioeconomic status and mental health."
+author: "Alimohammad Soufizadeh"
+date: "March, 2022"
+---
