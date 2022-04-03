899a5fd032f6242dd6b1ae4ed097454a81ea56bb
KianPinocchio <k.soofizadeh@icloud.com>
2022-04-03 21:37:22 +0200

    Tested: Works

--- Extented_Protocol_Analysis_SEM.R
+++ Extented_Protocol_Analysis_SEM.R
@@ -1,44 +1,110 @@
 ---
-title: "Data analysis for the extension protocol of study: 
-The interplay of cognitive reappraisal ability, 
-socioeconomic status and mental health."
-author: "Alimohammad Soufizadeh"
-date: "March, 2022"
+Title: "Data analysis for the extension protocol of study: The interplay of cognitive reappraisal ability, socioeconomic status and mental health."
+Author: "Alimohammad Soufizadeh"
+Date: "March, 2022"
 ---
+
+# Load packages
 pacman::p_load(pacman, tidyverse, kableExtra, psych, lavaan,
-               gridExtra,knitr, readxl, papaja, report)
+               gridExtra,knitr, readxl, papaja, report, mice, VIM, rmarkdown)
+
 
 # read all data
-raw_data <- read_excel("Coded_Combined.xlsx") #save the csv file as a dataframe named "all_data"
+raw_data <- read_excel("Coded_data.xlsx")
+
+# Change variable names
+raw_data <- rename(raw_data, 
+                   "duration" = "Duration(Second)", # in seconds
+                   "gender" = "Sex")
 
 # Handling Missing Data
-# 1. drop all NA
-clean_data <- drop_na(raw_data)
 
-# OR
-# 2. Drop NA skipping Age column
+# 1
+# 1. drop all NA
+# Run only if you want to run analysis with dropping all NA
+clean_data <- drop_na(raw_data) 
+
+# 2
+# 2. ALT way of handling Missing Data: Drop ALL NA skipping the Age column
 clean_data <- raw_data[complete.cases(raw_data[4:69]),]
 
-# Or
-# Impute data
+# 3. Impute missing data
+# Missing data pattern
+missing.pattern<-md.pattern(raw_data)
 
-'!!!LOADING!!!!'
+# visualise missing data
+library(VIM)
+mice_plot <- aggr(raw_data,numbers=TRUE, sortVars=TRUE,
+                    labels=names(raw_data), cex.axis=.7,
+                    gap=3, ylab=c("Missing data","Pattern"))
 
-# Check for duplicates
-sum(duplicated(clean_data))
+# Percentage of missing data per row
+no_missing <- raw_data
+percent_missing <- function(x){sum(is.na(x)/length(x)*100)}
 
-# Data summary
-summary(clean_data)
+missing <- apply(no_missing,1,percent_missing)
+table(missing)
 
-# Rename columns
-clean_data <- rename(clean_data, "gender" = "Sex")
+# Look for data to be imputed based on rows
+replace_rows <- subset(no_missing, missing <= 5)
+no_rows <- subset(no_missing, missing > 5)
 
-# Check demographics
-clean_data$gender <- factor(clean_data$gender, levels=c(1,2,3,4))
-summary (clean_data$gender)
+# Then look for data to be imputed based on columns
+missing <- apply(replace_rows, 2, percent_missing)
+table(missing)
+
+replace_columns <- replace_rows %>% select_if(missing<=5)
+no_columns <- replace_rows %>% select_if(missing > 5)
 
 
-#Score questionnaires:
+#IMPUTE DATA
+#Mice easily imputes, taking into account data type.
+tempnomiss <- mice(replace_columns)
+
+
+# Visualise missing data AGAIN!!!
+library(VIM)
+mice_plot <- aggr(replace_columns,numbers=TRUE, sortVars=TRUE,
+                  labels=names(replace_columns), cex.axis=1,
+                  gap=1, ylab=c("Missing data","Pattern"))
+
+# Get all the imputed data
+imputed_columns <- complete(tempnomiss)
+
+# Putting the data we dropped before back together with the rest
+all_columns <- cbind(no_columns, imputed_columns)
+'all_rows <- rbind(all_columns, no_rows)' # Only use if you want all other rows with NA binded as well
+
+# Outliers
+# Prepare for outlier detection
+columns = c("id", "Platform", "consent","duration", "Age","gender", "Edu")
+demographics <- subset(all_columns, select = columns) 
+all_columns <- all_columns[ , ! names(all_columns) %in% columns]
+imputed_data <- cbind(demographics, all_columns) # Save all clean data
+
+
+
+# Outliers Mahalanobis
+# Degrees of freedom in this case is based on the nr of variables (columns)
+# Detect outliers based on **unscored** data
+
+# Mahalanobis for unscored data
+mahal <- mahalanobis(imputed_data[ , -c(1:7)],
+  colMeans(imputed_data[ , -c(1:7)], na.rm=TRUE),
+  cov(imputed_data[ , -c(1:7)], use ="pairwise.complete.obs"))
+
+cutoff <- qchisq(p = 1 - .001, #1 minus alpha
+                 df = ncol(imputed_data[ , -c(1:7)])) # number of columns
+
+# Drop all outliers detected based on *unscored* data
+cutoff
+summary(mahal < cutoff) #notice the direction 
+no_outlier <- subset(imputed_data, mahal < cutoff)
+summary(no_outlier)
+
+
+# Score data
+# Prepare to score the data with temporarily no outliers
 
 # Create MCQ subscale variable groups
 mcq_pos <- c("MCQ1","MCQ7","MCQ10",
@@ -57,8 +123,11 @@ mcq_csc <- c("MCQ3","MCQ5","MCQ12",
              "MCQ16","MCQ18","MCQ30")
 
 
+# Function to Score data
+function_score_data <-function(no_outlier){
+
 #save variables in separate data frame
-scored_data <- clean_data %>%
+scored_data <- no_outlier %>%
   
   #row mean of the selected columns
   mutate(across(c("PSS2", "PSS3"), ~{6 - .}), # recode PSS items 2 & 3 (6 - response)
@@ -78,16 +147,66 @@ scored_data <- clean_data %>%
          sum_nc = rowSums(select(.,all_of(mcq_nc))),
          sum_csc = rowSums(select(.,all_of(mcq_csc))),
          sum_MCQ = rowSums(select(.,starts_with("MCQ"))))%>%
-  
-  
-  # select the scores/final variables used, remove the raw items
-  select(id, Age, SES, gender, mean_cra, mean_hcru,
-         sum_cesd, mean_pss, mean_bsm, sum_pos, sum_neg, sum_cc, sum_nc, sum_csc, sum_MCQ)
+return(scored_data)
+}
+
+# Score the data with no outliers
+scored_data <- function_score_data(no_outlier) #score data first, to find outliers among scored data
+
+
+# Summary of cleaned data 
+# Missing Data & outliers checked
+summary(scored_data)
+
+# Assumptions
+
+#Additivity is the assumption that each variable adds something to the model
+#You basically do not want to use the same variable twice, as that lowers power
+#Often this is described as multicollinearity (The problem is multicollineartiy). The assumption is called additivity.
+#Mainly, SEM analysis has a lot of correlated variables, you just want to make sure they aren't perfectly correlated
+
+
+# Additivity
+library(corrplot)
+plot_data <- cbind(scored_data$SES,scored_data[ , -c(1:69)])
+corrplot(cor(plot_data))
+
+
+# Assumptions Set Up
+random_variable <- rchisq(nrow(scored_data), 7)
+fake_model <- lm(scored_data$sum_cesd ~ ., # What's the difference if I regress on scored_data$sum_cesd ??
+                 data = scored_data[ , -c(1:69)])
+standardized <- rstudent(fake_model) # residuals
+fitvalues <- scale(fake_model$fitted.values) # z-score them
+
+# Linearity
+#We assume the the multivariate relationship between continuous variables is linear (i.e., no curved)
+#There are many ways to test this, but we can use a QQ/PP Plot to examine for linearity
+plot(fake_model, 2)
+
+# Normality
+#We expect that the residuals are normally distributed
+hist(standardized)
+
+#Assumptions Homogeneity and Homoscedasticity
+#These assumptions are about equality of the variances
+#Here the assumption is equality in the spread of variance across predicted values 
+{plot(standardized, fitvalues)
+  abline(v = 0)
+  abline(h = 0)
+}
+
+# Check for duplicates
+sum(duplicated(scored_data))
+
+# Check demographics
+scored_data$gender <- factor(scored_data$gender, levels=c(1,2,3,4)) # Factor gender
+summary(scored_data$gender)
 
 # Descriptives
-
-# In the first step, the data are summarized to get the descriptive statistics.
+#In the first step, the data are summarized to get the descriptive statistics.
 #Subsequently, the data are reformatted.
+
 descriptives <- scored_data %>% 
   dplyr::summarize(across(c(SES, Age, mean_cra, mean_hcru,sum_cesd, 
                             mean_pss, mean_bsm, sum_pos, 
@@ -117,10 +236,12 @@ descriptives <- scored_data %>%
          CSC = sum_csc, #
          MCQT = sum_MCQ) # MCQ =Meta Cognitions Questionnaire Total
 
+# Cronbach’s alphas
+#Select the items from the *raw* or *un-scored* data that belong to the specific scale.
+#calculate alpha and extract raw_alpha from the list the alpha function generates.
+
 # Calculate cronbach’s alphas
-# Select the items from the raw data that belong to the specific scale.
-# calculate alpha and extract raw_alpha from the list the alpha function generates.
-alpha <- clean_data %>%
+alpha <- scored_data %>%
   dplyr::summarize(
     # Replication Block Alphas
     cra_alpha = select(.,starts_with("CRA")) %>% psych::alpha() %>%
@@ -157,8 +278,8 @@ descriptives <- descriptives %>%
           POS = alpha$pos_alpha, NEG = alpha$neg_alpha, CC = alpha$cc_alpha,
           NC = alpha$nc_alpha, CSC = alpha$csc_alpha, MCQT = alpha$mcq_alpha)
 
-# make it a nicely formatted table
-library(rmarkdown)
+# Descriptives table
+# Make a nicely formatted table
 apa_table(descriptives) # is only shown when RMarkdown document is knitted
 
 # Plot monthly family income
@@ -166,8 +287,10 @@ income_plot<-hist(scored_data$SES,
                   main="Family income distribution",
                   xlab="family income category")
 
+# Mean centre all IVs
 # Mean centre all IVs for regressions with interaction terms
-centred_data <- scored_data %>%
+centre_data_to_csv <- function(scored_data){
+  centred_data <- scored_data %>%
   mutate(CRA_c = scale(mean_cra, center = TRUE, scale = FALSE),
          SES_c = scale(SES, center = TRUE, scale = FALSE),
          PSS_c = scale(mean_pss, center = TRUE, scale = FALSE),
@@ -178,69 +301,131 @@ centred_data <- scored_data %>%
          CC_c = scale(sum_cc, center = TRUE, scale = FALSE),
          NC_c = scale(sum_nc, center = TRUE, scale = FALSE),
          CSC_c = scale(sum_csc, center = TRUE, scale = FALSE),
-         MCQ_c = scale(sum_MCQ, center = TRUE, scale = FALSE))
+         MCQ_c = scale(sum_MCQ, center = TRUE, scale = FALSE))%>%
 
+  # select the scores/final variables used, remove the raw items
+  select(Age, gender, Edu, sum_cesd, SES_c , CRA_c, PSS_c, HCRU_c, BSM_c, POS_c, NEG_c, CC_c, CSC_c, MCQ_c)
 # Save prepared data
-write.csv(centred_data,"test_sem.csv", row.names = FALSE)
+write.csv(centred_data,"ready_for_analysis.csv", row.names = FALSE)
 
-# read data
-data <- read_csv("test_sem.csv")
+return(centred_data)
+}
 
-##### Three-way interaction #####
+# read prepared data for analysis
+# data <- read_csv("ready_for_analysis.csv") # Read csv data from computer
+# or
+ready_for_analysis <- centre_data_to_csv(scored_data)
 
-# Rename variables
-data <- data %>% mutate(rename(data,
-                               "Y" = "sum_cesd",
-                               "X" = "CRA_c",
-                               "W" = "SES_c",
-                               "Z" = "BSM_c",
-                               "COV" = "PSS_c"))
+# Hypotheses B1 & B2: Three-way interaction
+
+# Rename variables for convenience in modelling
+data <- ready_for_analysis %>% mutate(rename(ready_for_analysis,
+                                "X" = "CRA_c",
+                                "Y" = "sum_cesd",
+                                "W" = "SES_c",
+                                "Z" = "BSM_c",
+                                "COV" = "PSS_c",
+                               "M" = "NEG_c"))
 
 # Create interaction terms
 data <- data %>% mutate(X.W = X * W,
                         X.Z = X * Z,
                         W.Z = W * Z,
-                        X.W.Z = X * W * Z)
+                        X.W.Z = X * W * Z,
+                        M.W = M * W,
+                        M.Z = M * Z,
+                        M.W.Z = M * W * Z)
 
-# Create model
-mod.mod.model = 
-  "
-Y ~ a1 *COV + b1*X + b2*W + b3*Z + b4*X.W  + b5*X.Z + b6*W.Z + b7*X.W.Z
+# Three-way moderation model
+mod.mod.model = lm(Y ~ COV + X + W + Z + X*W  + X*Z + W*Z + X*W*Z, data = data)
+summary(mod.mod.model)
 
-#Mean of centred W (for use in simple slopes)
-Z ~ Z.mean*1
+# Check model fit
+performance::check_model(mod.mod.model)
 
-#Variance of centred W (for use in simple slopes)
-Z ~~ Z.c.var*Z
+# Simple slopes analysis
+interactions::sim_slopes(mod.mod.model, pred = X, modx=W, mod2 = Z, modxvals = NULL, jnalpha = 0.05, digits = 3, n.sd = 1)
 
-#Indirect effects conditional on moderator (b4 + b73*ModValue)
-indirect.SDL := b4 + b7 * (Z.mean - sqrt(Z.c.var))
-indirect.SDM := b4 + b7 * (Z.mean)
-indirect.SDH := b4 + b7 * (Z.mean + sqrt(Z.c.var))
+# Plot simple slopes for interaction
+interactions::interact_plot(mod.mod.model, pred = X, modx = W, mod2 = Z, centered = "none", y.label = "Cognitive Reappraisal Ability",x.label = "Socioeconomic Status", interval = TRUE, data = data)
+
+# Hypotheses C1 & C2: Moderated Mediation
+# The Model
+MOD.MED.model <-
 "
-# Fit model
-mod.mod.fit=sem(model=mmod.mod.model,
-        data=data,
-        se='bootstrap',
-        bootstrap=10)
+# Mediator
+M ~ a*X
 
-# Summarise measures
-summary(mod.mod.fit,fit.measures = FALSE,standardize = TRUE, rsquare = TRUE, ci = TRUE)
+# Outcome being predicted by the model
+Y ~ c1*X + c2*W + c3*Z + c4*X.W + c5*X.Z + c6*W.Z + c7*X.W.Z +
+b1*M + b2*W + b3*Z + b4*M.W + b5*M.Z + b6*W.Z + b7*M.W.Z + g*COV
+
+# Covariance: A main predictor's covariance is fixed so it does not 
+# covary with the rest of the variables, since the theory does not explicitely address this
+COV ~~ COV
+
+# Intercept and variance of W for conditional effects analysis
+W ~ W.mean*1
+W ~~ W.var * W
+
+# Intercept and variance of Z for conditional effects analysis
+Z ~ Z.mean*1
+Z ~~ Z.var*Z
+
+# Indirect effect of X on Y through M, conditional on mean values of W and Z
+indirect := (a) * ( b1 + b4*W.mean + b5*Z.mean)
+
+# Direct effect of X on Y, moderated by W and Z
+direct := c1 + c4*W.mean + c5*Z.mean
+
+# Total effect
+total := direct + indirect
+
+# Proportion of effect mediated
+prop.mediated := indirect / total
+
+# Conditional indirect effect through mediator (M)
+indirect.below :=(a)*(b1+b4*(W.mean - sqrt(W.var))+ b5*(Z.mean - sqrt(Z.var)))
+indirect.above :=(a)*(b1+b4*(W.mean + sqrt(W.var))+ b5*(Z.mean + sqrt(Z.var)))
+
+# Conditional direct effect moderated by W and Z
+direct.below:=c1 + c4*(W.mean - sqrt(W.var)) + c5*(Z.mean - sqrt(Z.var))
+direct.above:=c1 + c4*(W.mean + sqrt(W.var)) + c5*(Z.mean + sqrt(Z.var))
+
+# Conditional total effect
+total.below := direct.below + indirect.below
+total.above := direct.above + indirect.above
+
+# Proportion of mediation under conditions of W and one SD above or below mean
+prop.mediated.below := indirect.below / total.below
+prop.mediated.above := indirect.above / total.above
+"
+
+# Fit the model
+Mod.Med.SEM <- sem(model = MOD.MED.model,
+                   data = data,
+                   se = "bootstrap",
+                   bootstrap = 200) # do 5000 when actually running the code
+
+# Model summary
+summary(Mod.Med.SEM, fit.measures = FALSE, standardize = TRUE, rsquare = TRUE)
 
 # Parameters table
-parameterEstimates(mod.mod.fit, remove.nonfree = TRUE)
+parameterEstimates(Mod.Med.SEM, remove.nonfree = TRUE)
 
+
+# Plot the moderated mediation model
 # Plot model
-plot_4 = semPaths(mod.mod.fit, fixedStyle = 1, layout = "tree2", 
-                  intercepts = T, label.scale=T, nCharNodes = 0, 
-                  sizeMan2=3, sizeMan=7, asize=2, residuals = F, exoCov = F)
+library(semPlot)
+plot = semPaths(Mod.Med.SEM, fixedStyle = 1, layout = "tree", 
+                  intercepts = F, label.scale=T, nCharNodes = 0, 
+                  sizeMan2=3, sizeMan=7, asize=2, residuals = F, exoCov = T)
 
-# CLEAN UP #################################################
-
+# CLEAN UP
 # Clear environment
 rm(list = ls()) 
 
-# Clear packages
+# Clear packages 
 p_unload(all)
 
 # Clear console
